92fed02c92a2f5a16f92d8b17606c5c1683ad65d
Grace Kostanich <fl5812597@gmail.com>
2022-06-03 15:50:43 -0700

    Add all 5 questions

--- js/scripts.js
+++ js/scripts.js
@@ -6,71 +6,145 @@ $(document).ready(function() {
     const C = $("select#c").val();
     const python = $("select#python").val();
     
-  if (gender === "male" && swift === "yes" && C === "yes" && python === "yes") {
+  if (gender === "male" && swift === "yes" && C === "yes" && python === "yes" && noodle === "yes") {
     $("#language").text("JavaScript");
-  } else if (gender === "male" && swift === "yes" && C === "yes" && python === "no") {
+  } else if (gender === "male" && swift === "yes" && C === "yes" && python === "yes" && noodle === "no") {
+    $("#language").text("JavaScript");
+  } else if (gender === "male" && swift === "yes" && C === "yes" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "male" && swift === "yes" && C === "no" && python === "no") {
+  } else if (gender === "male" && swift === "yes" && C === "yes" && python === "no" && noodle === "no") {
+    $("#language").text("C#");
+  } else if (gender === "male" && swift === "yes" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("Swift");
-  } else if (gender === "male" && swift === "no" && C === "no" && python === "no") {
+  } else if (gender === "male" && swift === "yes" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("Swift");	
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "male" && swift === "yes" && C === "no" && python === "yes") {
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("C#");	
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");		
+  } else if (gender === "male" && swift === "yes" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "male" && swift === "no" && C === "yes" && python === "yes") {
+  } else if (gender === "male" && swift === "yes" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");	
+  } else if (gender === "male" && swift === "no" && C === "yes" && python === "yes" && noodle === "no") {
     $("#language").text("C#");
-  } else if (gender === "male" && swift === "no" && C === "no" && python === "yes") {
+  } else if (gender === "male" && swift === "no" && C === "yes" && python === "yes" && noodle === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "male" && swift === "no" && C === "yes" && python === "no") {
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "male" && swift === "no" && C === "yes" && python === "no" && noodle === "no") {
     $("#language").text("C#");
+  } else if (gender === "male" && swift === "no" && C === "yes" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");	
     /*Male*/
-  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "yes") {
+  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "yes" && noodle === "yes") {
     $("#language").text("JavaScript");
-  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "no") {
+  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "yes" && noodle === "no") {
+    $("#language").text("JavaScript");
+  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "female" && swift === "yes" && C === "no" && python === "no") {
+  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "no" && noodle === "no") {
+    $("#language").text("C#");
+  } else if (gender === "female" && swift === "yes" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("Swift");
-  } else if (gender === "female" && swift === "no" && C === "no" && python === "no") {
+  } else if (gender === "female" && swift === "yes" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("Swift");	
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "female" && swift === "yes" && C === "no" && python === "yes") {
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("C#");	
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");		
+  } else if (gender === "female" && swift === "yes" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "female" && swift === "no" && C === "yes" && python === "yes") {
+  } else if (gender === "female" && swift === "yes" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");	
+  } else if (gender === "female" && swift === "no" && C === "yes" && python === "yes" && noodle === "no") {
     $("#language").text("C#");
-  } else if (gender === "female" && swift === "no" && C === "no" && python === "yes") {
+  } else if (gender === "female" && swift === "no" && C === "yes" && python === "yes" && noodle === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "female" && swift === "no" && C === "yes" && python === "no") {
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "female" && swift === "no" && C === "yes" && python === "no" && noodle === "no") {
     $("#language").text("C#");
+  } else if (gender === "female" && swift === "no" && C === "yes" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");	
     /*Female*/ 
-  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "yes") {
+  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "yes" && noodle === "yes") {
     $("#language").text("JavaScript");
-  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "no") {
+  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "yes" && noodle === "no") {
+    $("#language").text("JavaScript");
+  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "no") {
+  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "no" && noodle === "no") {
+    $("#language").text("C#");
+  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("Swift");
-  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "no") {
+  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("Swift");	
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "yes") {
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("C#");	
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");		
+  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "yes") {
+  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");	
+  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "yes" && noodle === "no") {
     $("#language").text("C#");
-  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "yes") {
+  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "yes" && noodle === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "no") {
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "no" && noodle === "no") {
     $("#language").text("C#");
+  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");	
     /*NB*/
-  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "yes") {
+  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "yes" && noodle === "yes") {
     $("#language").text("JavaScript");
-  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "no") {
+  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "yes" && noodle === "no") {
+    $("#language").text("JavaScript");
+  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "other" && swift === "yes" && C === "no" && python === "no") {
+  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "no" && noodle === "no") {
+    $("#language").text("C#");
+  } else if (gender === "other" && swift === "yes" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("Swift");
-  } else if (gender === "other" && swift === "no" && C === "no" && python === "no") {
+  } else if (gender === "other" && swift === "yes" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("Swift");	
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
     $("#language").text("C#");
-  } else if (gender === "other" && swift === "yes" && C === "no" && python === "yes") {
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "no" && noodle === "no") {
+    $("#language").text("C#");	
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");		
+  } else if (gender === "other" && swift === "yes" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
-  } else if (gender === "other" && swift === "no" && C === "yes" && python === "yes") {
+  } else if (gender === "other" && swift === "yes" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");	
+  } else if (gender === "other" && swift === "no" && C === "yes" && python === "yes" && noodle === "no") {
     $("#language").text("C#");
-  } else if (gender === "other" && swift === "no" && C === "no" && python === "yes") {
+  } else if (gender === "other" && swift === "no" && C === "yes" && python === "yes" && noodle === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "yes" && noodle === "no") {
     $("#language").text("Python");
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "yes" && noodle === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "other" && swift === "no" && C === "yes" && python === "no" && noodle === "no") {
+    $("#language").text("C#");
+  } else if (gender === "other" && swift === "no" && C === "yes" && python === "no" && noodle === "yes") {
+    $("#language").text("C#");	
   } else {
     $("#language").text("C#");
   }
