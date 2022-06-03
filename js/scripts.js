203439465caeb44203f71761abf42f4009ae3323
Grace Kostanich <fl5812597@gmail.com>
2022-06-03 14:12:28 -0700

    WIP: Add if statements to control results

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,81 @@ $(document).ready(function() {
    */ 
 
     const gender = $("select#gender").val();
+    const swift = $("select#swift").val();
+    const C = $("select#c").val();
+    const python = $("select#python").val();
     
 
-    if (gender === "male") {
-      $("#language").text("MAN");
-    }
-    else if(gender === "female") {
-      $("#language").text("WOMAN");
-    }
-    else {
-      $("#language").text("COOL:)");
-    }
+  if (gender === "male" && swift === "yes" && C === "yes" && python === "yes") {
+    $("#language").text("JavaScript");
+  } else if (gender === "male" && swift === "yes" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "male" && swift === "yes" && C === "no" && python === "no") {
+    $("#language").text("Swift");
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "male" && swift === "yes" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "male" && swift === "no" && C === "yes" && python === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "male" && swift === "no" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "male" && swift === "no" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+    /**/
+  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "yes") {
+    $("#language").text("JavaScript");
+  } else if (gender === "female" && swift === "yes" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "female" && swift === "yes" && C === "no" && python === "no") {
+    $("#language").text("Swift");
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "female" && swift === "yes" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "female" && swift === "no" && C === "yes" && python === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "female" && swift === "no" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "female" && swift === "no" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+    /**/ 
+  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "yes") {
+    $("#language").text("JavaScript");
+  } else if (gender === "non-binary" && swift === "yes" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "no") {
+    $("#language").text("Swift");
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "non-binary" && swift === "yes" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "non-binary" && swift === "no" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "non-binary" && swift === "no" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+    /**/
+  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "yes") {
+    $("#language").text("JavaScript");
+  } else if (gender === "other" && swift === "yes" && C === "yes" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "other" && swift === "yes" && C === "no" && python === "no") {
+    $("#language").text("Swift");
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "no") {
+    $("#language").text("C#");
+  } else if (gender === "other" && swift === "yes" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else if (gender === "other" && swift === "no" && C === "yes" && python === "yes") {
+    $("#language").text("C#");
+  } else if (gender === "other" && swift === "no" && C === "no" && python === "yes") {
+    $("#language").text("Python");
+  } else (gender === "other" && swift === "no" && C === "yes" && python === "no"); {
+    $("#language").text("C#");
+  }
 
-
-    event.preventDefault();  
+  event.preventDefault();  
   });
 
   $("#hideResults").click(function(event) {
